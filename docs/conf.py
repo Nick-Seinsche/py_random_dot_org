c1ff1c8bd05b28e2699aacd8dda4f2ec7b8471c4
Nick Seinsche <nick.seinsche@gmail.com>
2024-09-12 23:08:14 +0200

    adding conf.py

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+import tomllib
+
+sys.path.insert(0, os.path.abspath(".."))
+
+with open("pyproject.toml", "rb") as f:
+    metadata = tomllib.load(f)
+
+project = metadata["project"]["name"]
+copyright = f"2024, {metadata['project']['authors'][0]['name']}"
+author = metadata["project"]["authors"][0]["name"]
+release = metadata["project"]["version"]
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
